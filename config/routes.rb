5d7e1c810c95627e485c7383c295225fc3bcb977
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2021-04-09 16:39:17 +0000

    End of Pt 1

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
         
   # READ
   get "/movies" => "movies#index"
-  get "/films/:id" => "movies#show", as: :movie # movie_path() (expects an argument to populate :id segment)
+  get "/movies/:id" => "movies#show", as: :movie # movie_path() (expects an argument to populate :id segment)
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
