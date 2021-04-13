11da25da8e4e8d82e6039612d8686936b6e7a5fd
jhoffmeyer <jhoffmeyer@chicagobooth.edu>
2021-04-13 20:16:11 +0000

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  #get "/", controller: "movies", action: "index" 
+
+  # get "/" => "movies#index"
+  
+  root "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
-          
+  post "/movies" => "movies#create", as: :movies #movies_url and movies_path 
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path 
+  
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+  get "/movies" => "movies#index" 
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() (expects and argument to populate :id segment)
   
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  patch "/movies/:id" => "movies#update" 
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #(expects and argument to populate :id segment)
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  delete "/movies/:id" => "movies#destroy" 
 
   #------------------------------
 end
