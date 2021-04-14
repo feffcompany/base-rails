7d5a454426c182bf546b1d3aa36999471e9effa9
jhoffmeyer <jhoffmeyer@chicagobooth.edu>
2021-04-14 14:55:00 +0000

    Final Changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,15 +29,15 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    
+    @movie = Movie.new(movie_attributes)
 
-    if movie.valid?
-      movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+    if @movie.valid?
+      @movie.save
+      redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new.html.erb"
+      render template: "new"
     end
   end
 
